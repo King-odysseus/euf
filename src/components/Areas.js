79c8f84a7230810f78553536f66de740b98905d9
Nate <96822493+King-odysseus@users.noreply.github.com>
2024-09-29 22:01:39 +0100

    new pages

---
+++ src/components/Areas.js
@@ -0,0 +1,16 @@
+// src/components/About.js
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container mt-4">
+      <h1>About EUF</h1>
+      <p>
+        EUF (Empowering Unity Foundation) is committed to creating meaningful
+        change around the globe through various initiatives.
+      </p>
+    </div>
+  );
+};
+
+export default About;
